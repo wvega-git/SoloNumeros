4adc578742e9b2d6dca1a13d3c26e8efe416c286
William Vega <wiro.vega@gmail.com>
2020-02-23 18:27:05 -0600

    Agregando un botoh

--- WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/Form1.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnClip = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -34,11 +35,21 @@ Partial Class Form1
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Mi primer carga en GitHub"
         '
+        'btnClip
+        '
+        Me.btnClip.Location = New System.Drawing.Point(15, 35)
+        Me.btnClip.Name = "btnClip"
+        Me.btnClip.Size = New System.Drawing.Size(75, 23)
+        Me.btnClip.TabIndex = 1
+        Me.btnClip.Text = "Dame un Clip"
+        Me.btnClip.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnClip)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -48,4 +59,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents btnClip As Button
 End Class
